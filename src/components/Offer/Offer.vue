7d1590cd637ebf2b4d679938f3879a6c3c10fe68
Ryan Waldon <ryanpwaldon@gmail.com>
2020-10-17 15:38:22 +1100

    feat: add offer card compoent

---
+++ src/components/Offer/Offer.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    class="relative flex flex-shrink-0 overflow-hidden whitespace-no-wrap transition duration-150 ease-in-out bg-white border border-transparent rounded-lg shadow focus:border-blue-300 focus:shadow-outline-blue focus:outline-none group"
+  >
+    <div class="flex-shrink-0 w-24 h-24 bg-center bg-cover" :style="{ backgroundImage: `url(${image})` }" />
+    <div class="flex self-center justify-between w-full p-4 space-x-4 overflow-hidden">
+      <div class="relative flex flex-col w-full overflow-hidden text-left">
+        <div class="absolute top-0 right-0 w-1/4 h-full bg-gradient-to-l from-white" />
+        <p class="text-sm font-medium leading-5 text-gray-900">{{ title }}</p>
+        <p class="text-xs leading-4 text-gray-500">{{ subtitle }}</p>
+      </div>
+      <div class="flex items-center transition duration-150 ease-in-out group-hover:opacity-0">
+        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+        </svg>
+      </div>
+    </div>
+    <div
+      class="absolute top-0 right-0 flex items-center justify-end w-7/12 h-full px-4 transition duration-150 ease-in-out opacity-0 bg-gradient-to-l from-gray-50 via-gray-50 group-hover:opacity-100"
+    >
+      <div class="grid gap-2 auto-rows-max">
+        <Button size="xs" text="View item" theme="white" />
+        <Button size="xs" text="Add to cart" theme="black" />
+      </div>
+    </div>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Button from '../Button/Button.vue'
+export default defineComponent({
+  components: {
+    Button
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      required: true
+    },
+    variantId: {
+      type: String,
+      required: false
+    },
+    variantUrl: {
+      type: String,
+      required: false
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,52 +1,44 @@
 <template>
   <div class="flex flex-col h-full">
-    <Header title="Your Cart" :meta="`${2} items`" class="z-20 flex-shrink-0 p-5 pb-0" />
+    <Header title="Your Cart" :meta="`${2} items`" class="flex-shrink-0 p-5 border-b border-gray-200" />
     <div class="relative overflow-scroll">
-      <div class="sticky top-0 z-10 w-full h-5 bg-gradient-to-b from-gray-50 via-gray-50" />
+      <div class="sticky top-0 z-10 w-full h-5" />
       <div class="grid gap-5 px-5 auto-rows-max">
         <LineItem
-          title="Georgia"
-          price="$99.00"
-          original-price="$109.00"
-          :variants="[
-            { name: 'Color', value: 'Black' },
-            { name: 'Size', value: 'A4' }
-          ]"
+          title="2021 Studio Art Diary"
+          price="$44.95"
+          :variants="[{ name: 'Size', value: 'A4' }]"
           image="https://cdn.shopify.com/s/files/1/0277/3070/6514/products/MG-2021_AGENDA_A5_ART_01.jpg?"
           quantity="1"
         />
         <LineItem
-          title="Georgia"
-          price="$99.00"
-          original-price="$109.00"
+          title="Urban Monochrome Drink Bottle"
+          price="$34.95"
+          original-price="$44.95"
           :variants="[
-            { name: 'Color', value: 'Cream' },
-            { name: 'Size', value: '36' }
+            { name: 'Color', value: 'Black' },
+            { name: 'Size', value: '500ml' }
           ]"
-          image="https://cdn.shopify.com/s/files/1/0277/3070/6514/products/MG-2021_AGENDA_A5_ART_01.jpg?"
+          image="https://cdn.shopify.com/s/files/1/0277/3070/6514/products/TB-8051513920066.01.jpg?v=1592390773"
           quantity="2"
         />
         <DividerLabel text="Offers" class="z-20 py-5" />
-        <LineItem
-          title="Georgia"
-          price="$99.00"
-          original-price="$109.00"
-          :variants="[{ name: 'Color', value: 'Cream' }]"
-          image="https://cdn.shopify.com/s/files/1/0277/3070/6514/products/MG-2021_AGENDA_A5_ART_01.jpg?"
+        <Offer
+          title="Add a graphite pencil"
+          subtitle="Only $4.99"
+          image="https://cdn.shopify.com/s/files/1/0277/3070/6514/products/CR-9020_large.jpg?v=159244352733"
           quantity="2"
         />
-        <LineItem
-          title="Georgia"
-          price="$99.00"
-          original-price="$109.00"
-          :variants="[{ name: 'Color', value: 'Cream' }]"
-          image="https://cdn.shopify.com/s/files/1/0277/3070/6514/products/MG-2021_AGENDA_A5_ART_01.jpg?"
+        <Offer
+          title="Add an infuser lid"
+          subtitle="Only $24.99"
+          image="https://cdn.shopify.com/s/files/1/0277/3070/6514/products/TB-8051513924095.01.jpg?v=1589864316"
           quantity="2"
         />
       </div>
-      <div class="sticky bottom-0 z-10 w-full h-5 bg-gradient-to-t from-gray-50 via-gray-50" />
+      <div class="sticky bottom-0 z-10 w-full h-5" />
     </div>
-    <div class="grid flex-shrink-0 gap-4 p-5 pt-0 mt-auto">
+    <div class="grid flex-shrink-0 gap-4 p-5 mt-auto border-t border-gray-200">
       <Balance subtotal="$109.00" />
       <Button text="Checkout" theme="black" />
     </div>
@@ -56,6 +48,7 @@
 <script lang="ts">
 import Header from '@/components/Header/Header.vue'
 import LineItem from '@/components/LineItem/LineItem.vue'
+import Offer from '@/components/Offer/Offer.vue'
 import Balance from '@/components/Balance/Balance.vue'
 import Button from '@/components/Button/Button.vue'
 import DividerLabel from '@/components/DividerLabel/DividerLabel.vue'
@@ -64,6 +57,7 @@ export default defineComponent({
   components: {
     Header,
     LineItem,
+    Offer,
     Balance,
     Button,
     DividerLabel
