dc31ea4721d18e1a8155b73eebf5ed8c028364e4
Ryan Waldon <ryanpwaldon@gmail.com>
2020-10-20 00:19:17 +1100

    feat: add sold out badge

--- src/components/InputVariant/InputVariant.vue
+++ src/components/InputVariant/InputVariant.vue
@@ -19,28 +19,41 @@
         </ListboxButton>
       </span>
       <transition leave-active-class="transition duration-100 ease-in" leave-from-class="opacity-100" leave-to-class="opacity-0">
-        <div v-if="open" class="absolute w-full mt-1 bg-white rounded-md shadow-lg">
-          <ListboxOptions
-            static
-            class="py-1 overflow-auto text-base leading-6 rounded-md shadow-xs max-h-60 focus:outline-none sm:text-sm sm:leading-5"
-          >
-            <ListboxOption v-for="(variant, i) in variants" :key="i" :value="variant.id" v-slot="{ selected, active }">
-              <div :class="`${active ? 'text-white bg-blue-600' : 'text-gray-900'} cursor-default select-none relative py-2 pl-8 pr-4`">
-                <span :class="`${selected ? 'font-semibold' : 'font-normal'} block truncate`">
-                  {{ variant.name }}
-                </span>
-                <span v-if="selected" :class="`${active ? 'text-white' : 'text-blue-600'} absolute inset-y-0 left-0 flex items-center pl-1.5`">
-                  <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path
-                      fill-rule="evenodd"
-                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </span>
-              </div>
-            </ListboxOption>
-          </ListboxOptions>
+        <div v-if="open" class="absolute w-full mt-1">
+          <div class="bg-white rounded-md shadow-lg">
+            <ListboxOptions
+              static
+              class="py-1 overflow-auto text-base leading-6 rounded-md shadow-xs max-h-60 focus:outline-none sm:text-sm sm:leading-5"
+            >
+              <ListboxOption
+                v-for="variant in variants"
+                :key="variant.id"
+                :value="variant.id"
+                :disabled="!variant.inStock"
+                v-slot="{ selected, active }"
+              >
+                <div
+                  class="relative py-2 pl-3 cursor-default select-none"
+                  :class="active ? 'text-white bg-blue-600 pr-9' : variant.inStock ? 'text-gray-900' : 'text-gray-400 pr-24'"
+                >
+                  <span :class="`${selected ? 'font-medium' : 'font-normal'} block truncate`">
+                    {{ variant.name }}
+                  </span>
+                  <span :class="`${active ? 'text-white' : 'text-blue-600'} absolute inset-y-0 right-0 flex items-center pr-4`">
+                    <svg v-if="selected" class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                    <Badge v-if="!variant.inStock" text="Sold Out" theme="gray-light" />
+                  </span>
+                </div>
+              </ListboxOption>
+            </ListboxOptions>
+          </div>
+          <div class="h-5" />
         </div>
       </transition>
     </div>
@@ -51,6 +64,7 @@
 import { Listbox, ListboxLabel, ListboxButton, ListboxOptions, ListboxOption } from '@headlessui/vue'
 import { computed, defineComponent, PropType } from 'vue'
 import { useField } from 'vee-validate'
+import Badge from '../Badge/Badge.vue'
 
 interface Variant {
   id: string | number
@@ -64,7 +78,8 @@ export default defineComponent({
     ListboxLabel,
     ListboxButton,
     ListboxOptions,
-    ListboxOption
+    ListboxOption,
+    Badge
   },
   props: {
     name: {
