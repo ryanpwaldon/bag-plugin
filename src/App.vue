0d5127b7d77cf5f0d24d30818420b3ee83d3cd91
Ryan Waldon <ryanpwaldon@gmail.com>
2020-10-20 01:33:58 +1100

    feat: add route animation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <Layout>
-    <component :is="name" v-bind="props" @route="handleRoute" />
+    <transition mode="out-in" @enter="enter" @leave="leave">
+      <component :is="name" v-bind="props" @route="handleRoute" />
+    </transition>
   </Layout>
 </template>
 
@@ -9,6 +11,7 @@ import { defineComponent } from 'vue'
 import Layout from './components/Layout/Layout.vue'
 import Home from './views/Home/Home.vue'
 import Edit from './views/Edit/Edit.vue'
+import anime from 'animejs'
 export default defineComponent({
   components: {
     Layout,
@@ -23,6 +26,28 @@ export default defineComponent({
     handleRoute({ name, props }: { name: string; props: object }) {
       this.name = name
       this.props = props
+    },
+    enter(el: HTMLElement, done: undefined) {
+      const translateX = this.name === 'Home' ? [-4, 0] : [4, 0]
+      anime({
+        targets: el,
+        translateX,
+        opacity: [0, 1],
+        easing: 'easeInOutQuad',
+        duration: 150,
+        complete: done
+      })
+    },
+    leave(el: HTMLElement, done: undefined) {
+      const translateX = this.name === 'Home' ? [0, 4] : [0, -4]
+      anime({
+        targets: el,
+        translateX,
+        opacity: [1, 0],
+        easing: 'easeInOutQuad',
+        duration: 150,
+        complete: done
+      })
     }
   }
 })
