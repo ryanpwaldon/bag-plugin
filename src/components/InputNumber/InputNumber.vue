28b267eb4ca80afa23b8b3f83d646a4fc49533fe
Ryan Waldon <ryanpwaldon@gmail.com>
2020-10-22 08:15:51 +1100

    feat: style updates

--- src/components/InputNumber/InputNumber.vue
+++ src/components/InputNumber/InputNumber.vue
@@ -4,7 +4,7 @@
     <span class="relative z-0 inline-flex w-full mt-1 shadow-sm">
       <button
         type="button"
-        class="relative inline-flex items-center px-2 py-2 text-sm font-medium leading-5 text-gray-400 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-l-md hover:text-gray-400 focus:z-20 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500"
+        class="relative inline-flex items-center px-2 py-2 text-sm font-medium leading-5 text-gray-400 transition duration-150 ease-in-out border border-gray-300 bg-gray-50 rounded-l-md hover:text-gray-400 focus:z-20 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500"
         aria-label="Minus"
         @click="decrement"
       >
@@ -23,7 +23,7 @@
       />
       <button
         type="button"
-        class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium leading-5 text-gray-400 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-r-md hover:text-gray-400 focus:z-20 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500"
+        class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium leading-5 text-gray-400 transition duration-150 ease-in-out border border-gray-300 bg-gray-50 rounded-r-md hover:text-gray-400 focus:z-20 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500"
         aria-label="Plus"
         @click="increment"
       >
