e12c8925449ffdfdc48a45814b172cdbb5a46e37
Ryan Waldon <ryanpwaldon@gmail.com>
2020-10-27 16:30:11 +1100

    updates

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,21 +1,8 @@
-import offerService, { Offer } from '@/services/api/services/offerService'
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-    offers: null as Offer[] | null
-  },
+  state: {},
   getters: {},
-  mutations: {
-    setOffers(state, value) {
-      state.offers = value
-    }
-  },
-  actions: {
-    async fetchOffers({ state, commit }) {
-      if (state.offers) return state.offers
-      commit('setOffers', await offerService.findAll())
-      return state.offers
-    }
-  }
+  mutations: {},
+  actions: {}
 })
