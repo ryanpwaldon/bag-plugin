a9b4c2b64cd1fc509db62b36c46e7cee346902b8
Ryan Waldon <ryanpwaldon@gmail.com>
2020-11-17 12:15:12 +1100

    feat: empty cart style

--- src/components/EmptyCart/EmptyCart.vue
+++ src/components/EmptyCart/EmptyCart.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex flex-shrink-0 overflow-hidden whitespace-no-wrap bg-white border rounded-lg shadow-sm border-true-gray-200">
-    <div class="flex-shrink-0 w-24 h-24 bg-center bg-cover" :style="{ backgroundImage: `url(${require('@/assets/img/empty-cart.svg')})` }" />
+    <div class="flex items-center flex-shrink-0 h-24 px-6">
+      <img class="w-10 h-10" src="@/assets/img/bag.svg" />
+    </div>
     <div class="flex flex-col items-start justify-center w-full p-4 pl-0 overflow-hidden">
       <p class="text-sm font-medium leading-5 text-true-gray-900">Your cart is empty</p>
-      <p class="text-xs leading-4 text-true-gray-500">Add some items to get started</p>
+      <p class="mt-1 text-xs leading-4 text-true-gray-500">Add some items to check out</p>
     </div>
   </div>
 </template>
