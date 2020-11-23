5f293472653884b7189596ab894272982c8eae9e
Ryan Waldon <ryanpwaldon@gmail.com>
2020-11-23 14:06:33 +1100

    feat: style

--- src/components/LineItem/LineItem.vue
+++ src/components/LineItem/LineItem.vue
@@ -5,7 +5,7 @@
   >
     <div class="flex-shrink-0 w-24 h-24 bg-center bg-cover" :style="{ backgroundImage: `url(${image})` }" />
     <div class="flex self-center justify-between w-full p-4 overflow-hidden text-xs leading-4 text-true-gray-500">
-      <div class="relative flex flex-col w-full overflow-hidden text-left">
+      <div class="relative flex flex-col w-full space-y-1 overflow-hidden text-left">
         <p
           class="text-sm font-medium leading-5 transition duration-150 ease-in-out text-true-gray-900"
           :class="!editMode && 'group-hover:text-true-gray-500'"
