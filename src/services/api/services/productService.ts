7079d157699c3ff174cae455725f625be3c8277e
Ryan Waldon <ryanpwaldon@gmail.com>
2020-12-30 12:14:27 +1100

    chore: cleanup

--- src/services/api/services/productService.ts
+++
@@ -1,9 +0,0 @@
-import api from '@/services/api/api'
-import { ServerProduct } from '@/types/serverApi'
-import { parseGid } from '@shopify/admin-graphql-api-utilities'
-
-export default {
-  async findOne(id: string): Promise<ServerProduct> {
-    return (await api({ url: `/product/${parseGid(id)}`, method: 'get' })).data
-  }
-}
