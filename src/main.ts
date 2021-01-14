679dfe87860e70ed2838bf7462e0494db1140a90
Ryan Waldon <ryanpwaldon@gmail.com>
2021-01-14 15:19:49 +1100

    feat: add sentry

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import App from '@/App.vue'
+import { createApp } from 'vue'
 import store from '@/store/store'
 import setup from '@/setup/setup'
-import scroll from '@/directives/scroll'
-import { createApp } from 'vue'
 import '@/assets/styles/index.css'
+import * as Sentry from '@sentry/vue'
+import scroll from '@/directives/scroll'
+import { Integrations } from '@sentry/tracing'
+
+Sentry.init({
+  tracesSampleRate: 1.0,
+  autoSessionTracking: true,
+  dsn: process.env.VUE_APP_SENTRY_DSN,
+  environment: process.env.VUE_APP_ENV,
+  integrations: [new Integrations.BrowserTracing()],
+  logErrors: process.env.VUE_APP_ENV !== 'production'
+})
 
 const start = async () => {
   const ready = await setup()
