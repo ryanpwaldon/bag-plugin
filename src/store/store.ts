02df0bc515f937676852112f506dc823b47b9bab
Ryan Waldon <ryanpwaldon@gmail.com>
2021-01-19 20:24:25 +1100

    chore: cleanup

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,6 @@
 import { createStore } from 'vuex'
-import { Permission } from '@/services/api/services/pluginService'
 
 export default createStore({
-  state: {
-    permissions: [] as Permission[]
-  },
-  mutations: {
-    setPermissions(state, value) {
-      state.permissions = value
-    }
-  }
+  state: {},
+  mutations: {}
 })
