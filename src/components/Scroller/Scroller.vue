e64962afd70bc73dd217b3c803bb7741407819d7
Ryan Waldon <ryanpwaldon@gmail.com>
2021-03-09 00:16:57 +1030

    browser fix: centre empty cart prompt

--- src/components/Scroller/Scroller.vue
+++ src/components/Scroller/Scroller.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col flex-1 px-5 overflow-auto xs:px-6">
     <div class="flex-shrink-0 w-full h-5 xs:h-6" />
-    <div class="relative flex-1">
+    <div class="relative flex flex-col flex-1">
       <slot />
     </div>
     <div class="flex-shrink-0 w-full h-5 xs:h-6" />
