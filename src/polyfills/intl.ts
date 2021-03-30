2b3c3667fbb93a6ae51f28e481cf1ca1c4569b46
Ryan Waldon <ryanpwaldon@gmail.com>
2021-03-30 17:20:05 +1100

    feat: add intl polyfill for narrowSymbol

--- src/polyfills/intl.ts
+++ src/polyfills/intl.ts
@@ -4,10 +4,6 @@ import { shouldPolyfill as shouldPolyfillGetCanonicalLocales } from '@formatjs/i
 import { shouldPolyfill as shouldPolyfillLocale } from '@formatjs/intl-locale/should-polyfill'
 import { shouldPolyfill as shouldPolyfillNumberFormat } from '@formatjs/intl-numberformat/should-polyfill'
 
-console.log(shouldPolyfillGetCanonicalLocales())
-console.log(shouldPolyfillLocale())
-console.log(shouldPolyfillNumberFormat())
-
 export const intlPolyfill = async () => {
   if (shouldPolyfillGetCanonicalLocales()) await import('@formatjs/intl-getcanonicallocales/polyfill')
   if (shouldPolyfillLocale()) await import('@formatjs/intl-locale/polyfill')
