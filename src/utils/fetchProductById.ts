cb31a619f69e444a7fcd5ad3304ea09bf998424d
Ryan Waldon <ryanpwaldon@gmail.com>
2021-04-25 17:34:55 +0930

    fix: getProductById is (sometimes) being called before parentFrame is connected

--- src/utils/fetchProductById.ts
+++ src/utils/fetchProductById.ts
@@ -1,5 +1,6 @@
-import { getParentFrame } from '@/composables/useParentFrame'
+import { getParentFrame, connectedToParentFrame } from '@/composables/useParentFrame'
 
 export default async (id: string) => {
+  await connectedToParentFrame
   return getParentFrame().getProductById(id)
 }
