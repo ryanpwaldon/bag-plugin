79d6ed4bb25b5ee93bef80d4d6aae4f8cb84dbfa
Ryan Waldon <ryanpwaldon@gmail.com>
2021-05-05 16:44:44 +0930

    fix: safari

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -6,7 +6,7 @@
       class="block w-full focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:outline-none"
     >
       <div class="flex items-stretch w-full">
-        <div class="flex-shrink-0 bg-center bg-cover xs:min-h-24 min-h-22 w-22 xs:w-24" :style="{ backgroundImage: `url(${image})` }" />
+        <div class="flex-shrink-0 bg-center bg-cover rounded-l xs:min-h-24 min-h-22 w-22 xs:w-24" :style="{ backgroundImage: `url(${image})` }" />
         <slot />
       </div>
     </component>
