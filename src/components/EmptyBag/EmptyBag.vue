7f0db2e41d0acd35c51a34783743c0bb45a19522
Ryan Waldon <ryanpwaldon@gmail.com>
2021-05-05 15:03:49 +0930

    fix: safari

--- src/components/EmptyBag/EmptyBag.vue
+++ src/components/EmptyBag/EmptyBag.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="flex flex-shrink-0 w-full overflow-hidden bg-white rounded shadow whitespace-nowrap xs:min-h-24 min-h-22">
-    <EmptyBag class="self-stretch flex-shrink-0 w-22 xs:w-24" />
-    <div class="flex flex-col justify-center w-full px-4 overflow-hidden">
+  <div class="flex items-stretch w-full overflow-hidden bg-white rounded shadow whitespace-nowrap xs:min-h-24 min-h-22">
+    <div class="flex-shrink-0 w-22 xs:w-24">
+      <EmptyBag class="w-full" />
+    </div>
+    <div class="flex flex-col items-start justify-center w-full px-4 overflow-hidden">
       <p class="text-sm font-medium text-gray-900 truncate">{{ $copy.emptyCartLine1 }}</p>
       <p class="mt-1 text-xs text-gray-500 truncate">{{ $copy.emptyCartLine2 }}</p>
     </div>
