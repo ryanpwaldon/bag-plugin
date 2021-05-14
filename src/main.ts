45e286bf0d811f3df1692413ba5e7bd20da66b68
Ryan Waldon <ryanpwaldon@gmail.com>
2021-05-14 15:39:46 +0930

    feat: add honeybadger

--- src/main.ts
+++ src/main.ts
@@ -3,32 +3,17 @@ import { createApp } from 'vue'
 import copy from '@/plugins/copy'
 import '@/assets/styles/index.css'
 import analytics from 'vue-gtag-next'
-import { Integrations } from '@sentry/tracing'
 import useOffers from '@/composables/useOffers'
 import { intlPolyfill } from '@/polyfills/intl'
+import honeybadger from '@/plugins/honeybadger'
 import 'focus-visible/dist/focus-visible.min.js'
-import { init as sentryInit, captureException, setTag } from '@sentry/browser'
 
 intlPolyfill()
 
-const app = createApp(App)
+export default createApp(App)
   .use(copy)
   .use(analytics, { property: { id: process.env.VUE_APP_GA_MEASUREMENT_ID } })
-
-app.config.errorHandler = (error, _, info) => {
-  setTag('info', info)
-  captureException(error)
-}
-
-sentryInit({
-  autoSessionTracking: true,
-  dsn: process.env.VUE_APP_SENTRY_DSN,
-  environment: process.env.VUE_APP_ENV,
-  integrations: [new Integrations.BrowserTracing()],
-  logLevel: process.env.VUE_APP_ENV === 'production' ? 0 : 3,
-  tracesSampleRate: process.env.VUE_APP_ENV === 'production' ? 0.25 : 1
-})
-
-app.mount('#app')
+  .use(honeybadger, { apiKey: process.env.VUE_APP_HONEYBADGER_API_KEY, environment: process.env.VUE_APP_ENV })
+  .mount('#app')
 
 useOffers().fetchOffers()
