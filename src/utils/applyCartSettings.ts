9738e4ade3fc7d4e3cbf152ebcdaadfa30ebf30d
Ryan Waldon <ryanpwaldon@gmail.com>
2021-07-26 00:35:39 +1000

    feat: updates

--- src/utils/applyCartSettings.ts
+++ src/utils/applyCartSettings.ts
@@ -1,4 +1,4 @@
-import { merge } from 'lodash'
+import merge from 'lodash/merge'
 import defaultCartSettings from '@/settings/defaultCartSettings'
 import { connectedToParentFrame, getParentFrame } from '@/composables/useParentFrame'
 
